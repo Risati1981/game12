d5a11780d61469d0acc0899a9a08fb2312cd8082
Risati1981 <128842824+Risati1981@users.noreply.github.com>
2023-03-24 11:41:43 -0700

    Update script.js
    rest of javascript

--- script.js
+++ script.js
@@ -51,4 +51,80 @@ function shuffleArray(array) {
 
 // Start the quiz
 function startQuiz() {
-  startButton.style.display =
+  startButton.style.display = "none";
+  quizContainer.style.display = "block";
+  shuffleArray(questions);
+  countdownElement.textContent = questions.length * 15;
+
+  // Start the countdown timer
+  countdownIntervalId = setInterval(() => {
+    const newCountdownValue = parseInt(countdownElement.textContent) - 1;
+    countdownElement.textContent = newCountdownValue;
+    if (newCountdownValue <= 0) {
+      endQuiz();
+    }
+  }, 1000);
+
+  showQuestion();
+}
+
+// Show a question
+function showQuestion() {
+  const currentQuestion = questions[questionIndex];
+  questionElement.textContent = currentQuestion.question;
+
+  // Shuffle the answer choices
+  shuffleArray(currentQuestion.choices);
+
+  // Add the answer choices to the buttons
+  for (let i = 0; i < choicesElement.children.length; i++) {
+    const button = choicesElement.children[i].querySelector(".choice");
+    button.textContent = currentQuestion.choices[i];
+    button.addEventListener("click", handleAnswerClick);
+  }
+}
+
+// Handle a click on an answer choice
+function handleAnswerClick(event) {
+  const selectedChoice = event.target;
+  const currentQuestion = questions[questionIndex];
+
+  if (currentQuestion.choices.indexOf(selectedChoice.textContent) === currentQuestion.correctChoice) {
+    // Answer is correct
+    score++;
+  } else {
+    // Answer is incorrect, deduct 10 seconds from the timer
+    const newCountdownValue = parseInt(countdownElement.textContent) - 10;
+    countdownElement.textContent = newCountdownValue;
+  }
+
+    // Move to the next question or end
+  questionIndex++;
+  if (questionIndex >= questions.length) {
+    endQuiz();
+  } else {
+    showQuestion();
+  }
+}
+
+// End the quiz
+function endQuiz() {
+  clearInterval(countdownIntervalId);
+  quizContainer.style.display = "none";
+  resultsContainer.style.display = "block";
+  scoreElement.textContent = score;
+}
+
+// Save the score and initials
+function saveScore() {
+  const initials = initialsInput.value.toUpperCase();
+  localStorage.setItem(initials, score);
+  initialsInput.value = "";
+  location.reload();
+}
+
+// Add event listener to start button
+startButton.addEventListener("click", startQuiz);
+
+// Add event listener to save button
+document.getElementById("save").addEventListener("click", saveScore);
